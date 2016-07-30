fba93f83d24cccbf4ff7d368594811273a447753
Piyush Chauhan <piyushchauhan2011@gmail.com>
2016-07-31 07:01:51 +1000

    Removed search box and improved styles with new autocomplete for search

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -386,44 +386,6 @@ function initMap() {
     });
     map.mapTypes.set('map_style', styledMap);
     map.setMapTypeId('map_style');
-
-    // Create the search box and link it to the UI element.
-    var input = document.getElementById('pac-input');
-    var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function() {
-        searchBox.setBounds(map.getBounds());
-    });
-
-    var markers = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    searchBox.addListener('places_changed', function() {
-        var places = searchBox.getPlaces();
-
-        if (places.length == 0) {
-        return;
-        }
-
-        // For each place, get the icon, name and location.
-        var bounds = new google.maps.LatLngBounds();
-        places.forEach(function(place) {
-            if (!place.geometry) {
-                console.log("Returned place contains no geometry");
-                return;
-            }
-
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
-        });
-        map.fitBounds(bounds);
-    });
     
     var marker_image = 'images/P_green.png';
     markers = [];
@@ -446,11 +408,28 @@ function initMap() {
     
 }
 
-angular.module('BlankApp', ['ngMaterial']);
+var app = angular.module('BlankApp', ['ngMaterial']);
+app.service('GeoCoderService', function($q) {
+    this.geoCodeAddress = function(address) {
+        this.geocoder = new google.maps.Geocoder();
+        var deferred = $q.defer();
+        this.geocoder.geocode({ address: address }, (results, status) => {
+            if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
+                deferred.resolve(results);
+            } else {
+                deferred.reject({
+                    data: 'No Results'
+                });
+            }
+        });
+        return deferred.promise;
+    }
+});
 
-angular
-  .module('BlankApp')
-  .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log, $q) {
+app.controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log, $q, GeoCoderService) {
+    $log.debug(GeoCoderService);
+    initMap();
+
     $scope.toggleLeft = buildToggler('left');
     $scope.isOpenLeft = function(){
       return $mdSidenav('left').isOpen();
@@ -468,65 +447,19 @@ angular
 
     $scope.simulateQuery = false;
     $scope.isDisabled    = false;
-    // list of `state` value/display objects
-    $scope.states        = loadAll();
+    
     $scope.querySearch   = querySearch;
     $scope.selectedItemChange = selectedItemChange;
     $scope.searchTextChange   = searchTextChange;
-    $scope.newState = newState;
-    function newState(state) {
-      alert("Sorry! You'll need to create a Constituion for " + state + " first!");
-    }
-    // ******************************
-    // Internal methods
-    // ******************************
-    /**
-     * Search for states... use $timeout to simulate
-     * remote dataservice call.
-     */
+    
     function querySearch (query) {
-      var results = query ? $scope.states.filter( createFilterFor(query) ) : $scope.states,
-          deferred;
-      if ($scope.simulateQuery) {
-        deferred = $q.defer();
-        $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-        return deferred.promise;
-      } else {
-        return results;
-      }
+      return GeoCoderService.geoCodeAddress(query);
     }
     function searchTextChange(text) {
       $log.info('Text changed to ' + text);
     }
     function selectedItemChange(item) {
       $log.info('Item changed to ' + JSON.stringify(item));
-    }
-    /**
-     * Build `states` list of key/value pairs
-     */
-    function loadAll() {
-      var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
-              Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
-              Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
-              Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
-              North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
-              South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
-              Wisconsin, Wyoming';
-      return allStates.split(/, +/g).map( function (state) {
-        return {
-          value: state.toLowerCase(),
-          display: state
-        };
-      });
-    }
-    /**
-     * Create filter function for a query string
-     */
-    function createFilterFor(query) {
-      var lowercaseQuery = angular.lowercase(query);
-      return function filterFn(state) {
-        return (state.value.indexOf(lowercaseQuery) === 0);
-      };
     }
   })
   .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
