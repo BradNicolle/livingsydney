4cbc27fcc8af730860f9710df967787698420955
Piyush Chauhan <piyushchauhan2011@gmail.com>
2016-07-31 05:12:50 +1000

    Inclusion of google autocomplete

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -377,7 +377,7 @@ var styles = [
     }
 ];
 
-function initMap() {
+function initMap() {     
     var styledMap = new google.maps.StyledMapType(styles, { name: "Styled Map" });
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -33.865627, lng: 151.207275 },
@@ -387,8 +387,46 @@ function initMap() {
     map.mapTypes.set('map_style', styledMap);
     map.setMapTypeId('map_style');
 
-    var marker_image = 'images/P_green.png';
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
     var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+        return;
+        }
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+    
+    var marker_image = 'images/P_green.png';
+    markers = [];
 
     $.getJSON('/parking/disabled', function(data) {
         for (i = 0; i < data.length; i++) {
