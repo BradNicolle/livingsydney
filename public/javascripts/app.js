6745f851b623307a9bc420bc6d98f0905ee3a0a7
Piyush Chauhan <piyushchauhan2011@gmail.com>
2016-07-31 05:43:34 +1000

    Initial structure of layout with map

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -445,3 +445,33 @@ function initMap() {
     });
     
 }
+
+angular.module('BlankApp', ['ngMaterial']);
+
+angular
+  .module('BlankApp')
+  .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.toggleLeft = buildToggler('left');
+    $scope.isOpenLeft = function(){
+      return $mdSidenav('left').isOpen();
+    };
+    function buildToggler(navID) {
+      return function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }
+    }
+  })
+  .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+      // Component lookup should always be available since we are not using `ng-if`
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
+    };
+  });
